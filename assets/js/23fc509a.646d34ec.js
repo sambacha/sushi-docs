0179cb61d1432c144d879280681c74787044e6ce
sambacha <sam@freighttrust.com>
2022-05-23 05:31:17 +0000

    Deploying to gh-pages from @ sambacha/sushi-docs@9faeaf2b31fc919ad6624c165fc467f72fc6c20b 🚀

---
+++ assets/js/23fc509a.646d34ec.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksushi_docs=self.webpackChunksushi_docs||[]).push([[1683],{3769:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
---
+++ assets/js/8a4e69c7.0943bbd3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksushi_docs=self.webpackChunksushi_docs||[]).push([[7511],{5745:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
